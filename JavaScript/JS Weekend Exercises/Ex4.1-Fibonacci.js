02e9eb5acb55fe524c8b8ca815128b5d7555b976
Sergey Aronov <AronovSergeyy@gmail.com>
2021-02-18 15:57:25 +0200

    JS Weekend Exercises

--- JavaScript/JS Weekend Exercises/Ex4.1-Fibonacci.js
+++ JavaScript/JS Weekend Exercises/Ex4.1-Fibonacci.js
@@ -7,13 +7,18 @@
 */
 
 const fibonacci = (n) => {
+    if(n === 1) return 0;
+    else if(n === 2) return 1;
+
     let prev = 0;
     let curr = 1;
+    
     for(let i = 2; i < n; i++) {
         const next = prev + curr;
         prev = curr;
         curr = next;
     }
+
     return curr;
 }
 
