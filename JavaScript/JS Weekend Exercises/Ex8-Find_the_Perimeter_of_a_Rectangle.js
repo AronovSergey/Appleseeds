a5406e7e96e2816f206a9c3db37c4d32a78767c5
Sergey Aronov <AronovSergeyy@gmail.com>
2021-02-20 17:13:21 +0200

    JS Weekend Exercises

---
+++ JavaScript/JS Weekend Exercises/Ex8-Find_the_Perimeter_of_a_Rectangle.js
@@ -0,0 +1,11 @@
+/*
+    Create a function that takes length and width and finds the perimeter of a rectangle.
+*/
+const findPerimeter = (length, width) => length + length + width + width;
+
+// Examples 
+console.log(findPerimeter(6, 7));
+console.log(findPerimeter(20, 10));
+console.log(findPerimeter(2, 10));
+
+
