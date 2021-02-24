57f16001bcc3895df63ada7c6de19c39dbc4a32c
Sergey Aronov <AronovSergeyy@gmail.com>
2021-02-24 11:01:04 +0200

    22.3 submission

--- JavaScript/22.3-guess_my_letter/index.js
+++ JavaScript/22.3-guess_my_letter/index.js
@@ -1,6 +1,7 @@
 const keys = document.querySelector(".keys");
 const guess = document.querySelector(".guess");
 const guidance = document.querySelector(".guidance");
+const h2 = document.querySelector("h2");
 const characters = 'abcdefghijklmnopqrstuvwxyz';
 const randomChar = characters.charAt(Math.floor(Math.random() * characters.length));
 const guessArr = [];
@@ -14,6 +15,8 @@ document.body.addEventListener('keyup', (e) => {
         guess.textContent = key;
         guidance.textContent = "Right Letter!";
         guidance.style.color = "green";
+        h2.textContent = 'Would you like to play again?';
+        keys.outerHTML = `<button onclick="rerender()">Yes</button>`;
     }
     else {
         if(!guessArr.includes(key)){
@@ -26,4 +29,8 @@ document.body.addEventListener('keyup', (e) => {
         }
         guidance.style.color = "red";
     }
-})
+});
+
+function rerender() {
+    window.location.reload();
+}
