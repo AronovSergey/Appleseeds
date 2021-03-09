7370befef00ca3b9e476d9426f973f79e37795b5
Sergey Aronov <AronovSergeyy@gmail.com>
2021-03-09 13:49:41 +0200

    To Do Lists Submission

---
+++ To Do Lists/script/List.js
@@ -0,0 +1,65 @@
+class ListItem {
+    static id = 0; 
+
+    constructor(_content) {
+        this.id = ListItem.id++;
+        this.content = _content;
+        this.completed = false;
+    }
+
+    getId() { return this.id; }
+    getContent() { return this.content; }
+    getCompleted() { return this.completed; }
+    setContent(_content) { this.content = _content; }
+    changeCompleted() { !this.completed; }
+
+}
+
+class List {
+    constructor() {
+        this.list = [];
+    }
+
+    findIndex(id) {
+        return this.list.map(item => item.getId()).indexOf(id);
+    }
+
+    create(content) {
+        this.list.push(new ListItem(content));
+    }
+
+    read(id) {
+        const index = this.findIndex(id);
+        if(index > -1){
+            return {
+                id, 
+                content: this.list[index].getContent(),
+                completed: this.list[index].getCompleted(),
+            }
+        }
+        return false;
+    }
+
+    update(id, content) {
+        const index = this.findIndex(id);
+        if(index > -1){
+            this.list[index].setContent(content);
+            return true;
+        }
+        return false;
+    }
+
+    delete(id) {
+        const index = this.findIndex(id);
+        if(index > -1){
+            this.list.splice(index, 1);
+            return true;
+        }
+        return false;
+    }
+
+    getAll() { return this.list; }
+}
+
+const list = new List();
+
