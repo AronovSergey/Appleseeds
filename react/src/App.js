9f5775541c5cb945d42220b8ed5648019edeca70
Sergey Aronov <AronovSergeyy@gmail.com>
2021-03-14 14:03:18 +0200

    2.2 submission

--- react/src/App.js
+++ react/src/App.js
@@ -1,8 +1,14 @@
-import HelloWorld from './components/2.1-hello_world/HelloWorld'
+//import HelloWorld from './components/2.1-hello_world/HelloWorld';
+//import BasicOfJSX from './components/2.2-basics_of_jsx/BasicOfJSX'
 
 function App() {
   return (
-    <HelloWorld />
+    // ------------------- 2.1 -------------------
+    //<HelloWorld />
+
+    // ------------------- 2.2 -------------------
+    //<BasicOfJSX />
+    
   );
 }
 
